cfc56b93feef0990a983321f7a4c825ef7c1eee0
Froris <gamefoxmail@gmail.com>
2020-05-22 16:08:44 +0300

    bug fix

--- src/views.js
+++ src/views.js
@@ -4,72 +4,74 @@ import { getFilters } from "./filters.js";
 
 // Создание DOM структуры для заметки
 const generateNoteDOM = (note) => {
-    const noteEl = document.createElement('div');
-    const textEl = document.createElement('a');
-    const button = document.createElement('button');
-    const editButton = document.createElement('a');
+  const noteEl = document.createElement("div");
+  const textEl = document.createElement("a");
+  const button = document.createElement("button");
+  const editButton = document.createElement("a");
 
-    // Кнопка удаления
-    button.classList.add('remove-button');
-    button.setAttribute('title', 'remove note')
-    noteEl.appendChild(button);
-    button.addEventListener('click', () => {
-        removeNote(note.id);
-        saveNotes();
-        renderNotes();
-    })
+  // Кнопка удаления
+  button.classList.add("remove-button");
+  button.setAttribute("title", "remove note");
+  noteEl.appendChild(button);
+  button.addEventListener("click", () => {
+    removeNote(note.id);
+    saveNotes();
+    renderNotes();
+  });
 
-    // Кнопка редактирвоания
-    editButton.setAttribute('href', `/edit.html#${note.id}`);
-    editButton.classList.add('edit-button');
-    editButton.textContent = 'EDIT';
-    noteEl.appendChild(editButton);
+  // Кнопка редактирвоания
+  editButton.setAttribute("href", `/edit.html#${note.id}`);
+  editButton.classList.add("edit-button");
+  editButton.textContent = "EDIT";
+  noteEl.appendChild(editButton);
 
-    // Заголовок заметки
-    if (note.title.length > 0) {
-        textEl.textContent = note.title;
-    } else {
-        textEl.textContent = 'Unnamed note';
-    }
+  // Заголовок заметки
+  if (note.title.length > 0) {
+    textEl.textContent = note.title;
+  } else {
+    textEl.textContent = "Unnamed note";
+  }
 
-    noteEl.classList.add('notes__controls');
-    textEl.classList.add('note__text');
-    noteEl.appendChild(textEl);
+  noteEl.classList.add("notes__controls");
+  textEl.classList.add("note__text");
+  noteEl.appendChild(textEl);
 
-    return noteEl;
-}
+  return noteEl;
+};
 
 // Вывод заметок на экран
 const renderNotes = () => {
-    const notesEl = document.querySelector('#notes');
-    const filters = getFilters()
-    const notes = sortNotes(filters.sortBy);
-    const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()));
+  let notesEl = document.querySelector("#notes");
+  const filters = getFilters();
+  const notes = sortNotes(filters.sortBy);
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  );
 
-    notesEl.innerHTML = '';
+  notesEl.innerHTML = "";
 
-    filteredNotes.forEach((note) => {
-        const noteEl = generateNoteDOM(note);
-        notesEl.appendChild(noteEl);
-    });
+  filteredNotes.forEach((note) => {
+    const noteEl = generateNoteDOM(note);
+    notesEl.appendChild(noteEl);
+  });
 };
 
 const initializedEditPage = (noteId, titleElement, bodyElement, dateElement) => {
-    const notes = getNotes();
-    const note = notes.find((note) => note.id === noteId);
+  const notes = getNotes();
+  const note = notes.find((note) => note.id === noteId);
 
-    if (!note) {
-    location.assign('/index.html');
-    }
+  if (!note) {
+    location.assign("/index.html");
+  }
 
-    titleElement.value = note.title;
-    bodyElement.value = note.body;
-    dateElement.textContent = generateLastEdited(note.updatedAt);
-}
+  titleElement.value = note.title;
+  bodyElement.value = note.body;
+  dateElement.textContent = generateLastEdited(note.updatedAt);
+};
 
 // Последнее обновление заметки
 const generateLastEdited = (timestamp) => {
-    return `Last edited: ${moment(timestamp).fromNow()}`;
+  return `Last edited: ${moment(timestamp).fromNow()}`;
 };
 
 export { generateNoteDOM, renderNotes, generateLastEdited, initializedEditPage };
